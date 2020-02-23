03fbe306fb1467290564e4ef8fd7e5d19e40191b
Anas Juwaidi <anas.didi95@gmail.com>
2020-02-23 20:50:05 +0800

    src/pages/index.tsx modified

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,6 +12,7 @@ import { useStaticQuery, graphql } from "gatsby"
 import { AppQuery } from "../graphqlTypes"
 import Img, { FixedObject } from "gatsby-image"
 import { oc } from "ts-optchain"
+import Header, { IDetailHeader, ISocialHeader } from "../components/Header"
 
 interface IApp {}
 
@@ -25,63 +26,36 @@ const App: React.FC<IApp> = () => {
           }
         }
       }
+      header: resumeJson {
+        fullname
+        position
+        detail {
+          dateOfBirth
+          place
+          phone
+          email
+        }
+        social {
+          github
+          linkedin
+        }
+        summary
+      }
     }
   `)
 
+  const { profileImg, header } = data
+
   return (
     <div className="resume">
-      <section className="flex">
-        <article className="w-3/12">
-          <Img
-            className="rounded-full"
-            fixed={oc(data).profileImg.childImageSharp.fixed() as FixedObject}
-          />
-          <div className="mt-1 inline-flex">
-            <TiCalendar />
-            <p className="text-sm font-semibold ml-2">27 Aug 1995</p>
-          </div>
-          <div className="mt-1 inline-flex">
-            <TiLocation />
-            <p className="text-sm font-semibold ml-2">Selangor, Malaysia</p>
-          </div>
-          <div className="mt-1 inline-flex">
-            <TiDevicePhone />
-            <p className="text-sm font-semibold ml-2">018-7601343</p>
-          </div>
-          <div className="mt-1 inline-flex">
-            <TiMail />
-            <p className="text-sm font-semibold ml-2">anas.didi95@gmail.com</p>
-          </div>
-        </article>
-        <article className="ml-5 w-full">
-          <h1 className="font-bold capitalize text-3xl mb-2">
-            Anas Juwaidi Bin mohd jeffry
-          </h1>
-          <h2 className="text-semibold text-xl text-gray-700 italic">
-            Software Engineer
-          </h2>
-          <div className="flex justify-around mt-5 items-baseline">
-            <span className="inline-flex">
-              <TiSocialGithubCircular className="text-xl" />{" "}
-              <p className="text-sm font-semibold ml-1">
-                github.com/anas-didi95
-              </p>
-            </span>
-            <span className="inline-flex">
-              <TiSocialLinkedinCircular className="text-xl" />{" "}
-              <p className="text-sm font-semibold ml-1">
-                linkedin.com/in/anas-juwaidi-mohd-jeffry
-              </p>
-            </span>
-          </div>
-          <p className="text-justify p-4 bg-gray-200 rounded-lg mt-6 h-32">
-            Telecom Engineer with IT experience. Worked in the most important
-            telecom company in Malaysia across different areas. Got experience
-            managing servers, application servers and cloud tools. Love learning
-            new things or tools everyday.
-          </p>
-        </article>
-      </section>
+      <Header
+        profileImg={oc(profileImg).childImageSharp.fixed() as FixedObject}
+        fullname={oc(header).fullname("")}
+        detail={oc(header).detail() as IDetailHeader}
+        position={oc(header).position("")}
+        social={oc(header).social() as ISocialHeader}
+        summary={oc(header).summary([]) as string[]}
+      />
     </div>
   )
 }
