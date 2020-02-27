8fca4d16cbaa2b021a3cb25e163c332b18817a85
Anas Juwaidi <anas.didi95@gmail.com>
2020-02-27 08:37:20 +0800

    src/components/Education.tsx modified

--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -18,17 +18,17 @@ const Education: React.FC<IEducation> = ({
   period,
   mark,
 }) => (
-  <article className="mb-3">
-    <div className="flex justify-between">
-      <h1 className="text-orange-500 text-xl">{major}</h1>
-    </div>
-    <p className="italic mt-1 font-bold">{university}</p>
-    <p className="text-sm mt-1 italic">{period}</p>
-    <p className="text-sm mt-1">
-      <span className="font-bold">{mark.name}:</span>&nbsp;{mark.value}
-    </p>
-    <br />
-  </article>
-)
+    <article className="mb-3">
+      <div className="flex justify-between">
+        <h1 className="text-orange-500 text-lg">{major}</h1>
+      </div>
+      <p className="italic mt-1 font-bold">{university}</p>
+      <p className="text-sm mt-1 italic">{period}</p>
+      <p className="text-sm mt-1">
+        <span className="font-bold">{mark.name}:</span>&nbsp;{mark.value}
+      </p>
+      <br />
+    </article>
+  )
 
 export default Education
