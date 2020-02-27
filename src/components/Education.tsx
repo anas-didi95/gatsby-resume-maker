6abefe11788fc59957f44f9ac71d15ebce2ca13c
Anas Juwaidi <anas.didi95@gmail.com>
2020-02-27 08:19:26 +0800

    src/components/Education.tsx modified

--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 
-interface IEducationMark {
+export interface IEducationMark {
   name: string
   value: string
 }
@@ -8,16 +8,27 @@ interface IEducationMark {
 interface IEducation {
   major: string
   university: string
+  period: string
   mark: IEducationMark
 }
 
-const Education: React.FC<IEducation> = ({ major, university, mark }) => (
+const Education: React.FC<IEducation> = ({
+  major,
+  university,
+  period,
+  mark,
+}) => (
   <article className="mb-3">
     <div className="flex justify-between">
       <h1 className="text-orange-500 text-xl">{major}</h1>
     </div>
     <p className="italic mt-1 font-bold">{university}</p>
-    <p className="text-sm mt-1"><span className="font-bold">{mark.name}</span>&nbsp;{mark.value}</p>
+    <p className="text-sm mt-1 italic">{period}</p>
+    <p className="text-sm mt-1">
+      <span className="font-bold">{mark.name}:</span>&nbsp;{mark.value}
+    </p>
     <br />
   </article>
 )
+
+export default Education
