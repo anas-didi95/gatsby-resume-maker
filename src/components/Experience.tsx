8faf6bcef8c8ba427ce2fd0e9aac3fabc4edca3c
Anas Juwaidi <anas.didi95@gmail.com>
2020-02-27 08:19:26 +0800

    src/components/Experience.tsx modified

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -13,19 +13,19 @@ const Experience: React.FC<IExperience> = ({
   period,
   achievements,
 }) => (
-    <article className="mb-3">
-      <div className="flex justify-between">
-        <h1 className="text-orange-500 text-xl">{position}</h1>
-        <h1 className="text-orange-500 text-xl">{period}</h1>
-      </div>
-      <p className="italic mt-1 font-bold">{company}</p>
-      <p className="mt-2">Achievements / Tasks</p>
-      <ul className="list-disc ml-6">
-        {achievements.map((ach, idx) => (
-          <li className="text-sm mt-1">{ach}</li>
-        ))}
-      </ul>
-    </article>
-  )
+  <article className="mb-3">
+    <div className="flex justify-between">
+      <h1 className="text-orange-500 text-xl">{position}</h1>
+      <h1 className="text-orange-500 text-xl">{period}</h1>
+    </div>
+    <p className="italic mt-1 font-bold">{company}</p>
+    <p className="mt-2">Achievements / Tasks</p>
+    <ul className="list-disc ml-6">
+      {achievements.map((ach, idx) => (
+        <li className="text-sm mt-1">{ach}</li>
+      ))}
+    </ul>
+  </article>
+)
 
 export default Experience
