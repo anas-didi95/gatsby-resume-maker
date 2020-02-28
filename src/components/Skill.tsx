ada7fbdba42dd6eb8d7bfad9106977c127f4d3b6
Anas Juwaidi <anas.didi95@gmail.com>
2020-02-28 08:14:39 +0800

    src/components/Skill.tsx added

---
+++ src/components/Skill.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+interface ISkill {
+  name: string,
+  proficient: boolean
+}
+
+const Skill: React.FC<ISkill> = ({ name, proficient }) => (
+  <button className={`border px-4 py-2 rounded-full m-1 ${proficient ? "bg-green-300" : "bg-yellow-300"}`}>
+    {name}
+  </button>
+)
+
+export default Skill
