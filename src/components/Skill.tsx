129edf0bf4ce8c30a5fdae80d8ba401d614893f1
Anas Juwaidi <anas.didi95@gmail.com>
2020-02-28 08:15:05 +0800

    src/components/Skill.tsx modified

--- src/components/Skill.tsx
+++ src/components/Skill.tsx
@@ -1,12 +1,15 @@
 import React from "react"
 
 interface ISkill {
-  name: string,
+  name: string
   proficient: boolean
 }
 
 const Skill: React.FC<ISkill> = ({ name, proficient }) => (
-  <button className={`border px-4 py-2 rounded-full m-1 ${proficient ? "bg-green-300" : "bg-yellow-300"}`}>
+  <button
+    className={`border px-4 py-2 rounded-full m-1 ${
+      proficient ? "bg-green-300" : "bg-yellow-300"
+    }`}>
     {name}
   </button>
 )
