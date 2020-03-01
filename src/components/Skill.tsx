dfdea14ac7448159885094ae4c38cb14c3bec0b1
Anas Juwaidi <anas.didi95@gmail.com>
2020-03-01 17:56:30 +0800

    src/components/Skill.tsx modified

--- src/components/Skill.tsx
+++ src/components/Skill.tsx
@@ -7,7 +7,7 @@ interface ISkill {
 
 const Skill: React.FC<ISkill> = ({ name, proficient }) => (
   <button
-    className={`border px-4 py-2 rounded-full m-1 text-xs font-semibold ${
+    className={`border px-4 py-2 rounded-full mx-1 mt-1 text-xs font-semibold ${
       proficient ? "bg-green-300" : "bg-yellow-300"
       }`}>
     {name}
