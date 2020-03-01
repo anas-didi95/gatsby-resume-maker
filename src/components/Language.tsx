25388664cad5c1a23edce6cfc57459b1d2774674
Anas Juwaidi <anas.didi95@gmail.com>
2020-03-01 11:56:54 +0800

    src/components/Language.tsx modified

--- src/components/Language.tsx
+++ src/components/Language.tsx
@@ -6,10 +6,10 @@ interface ILanguage {
 }
 
 const Language: React.FC<ILanguage> = ({ name, level }) => (
-  <>
+  <div className="inline-flex justify-between w-full items-center mt-1">
     <span className="font-bold">{name}</span>
     <span className="italic">{level}</span>
-  </>
+  </div>
 )
 
 export default Language
